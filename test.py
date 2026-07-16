ceebee6728b65aee9333731aa7b346a5e0f4dccb
khoa16122004 <khoatr.work@gmail.com>
2026-07-16 14:39:01 +0700

    :a;

--- test.py
+++ test.py
@@ -626,9 +626,9 @@ def evaluate_method(method, args, entries, folder_to_label, clip_model, explaine
         except Exception:
             continue
         
-        print("Khoa")
-        raise
+
         if args.save_per_sample_plots:
+            print('Khoa')
             sample_folder = os.path.dirname(entry["metadata_path"])
             sample_plot_name = f"{args.output_prefix}_{method}_sample_auc.png"
             try:
