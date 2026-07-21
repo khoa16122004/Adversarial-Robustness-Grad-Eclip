f02ba401f151ea27a641e5e45a5c7cc7b737364c
khoa16122004 <khoatr.work@gmail.com>
2026-07-22 00:39:45 +0700

    a'

--- RISE/evaluation.py
+++ RISE/evaluation.py
@@ -248,7 +248,7 @@ class AdversarialCausalMetric(CausalMetric):
                 xt = x_raw_adv
                 finish = self.substrate_fn(x_raw_adv)
             elif self.mode == 'ins':
-                print(x_raw_adv)
+                print(self.substrate_fn)
                 xt = self.substrate_fn(x_raw_adv)
                 finish = x_raw_adv
                 print(xt)
